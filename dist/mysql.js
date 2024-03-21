b2a0b01091b8d3dd4ed6d76bc7d0c9a018dc7bf4
Matheus Ramazza <01ramazza@gmail.com>
2024-03-20 23:17:08 -0300

    Refactor

--- dist/mysql.js
+++ dist/mysql.js
@@ -7,12 +7,12 @@ exports.pool = void 0;
 const mysql_1 = __importDefault(require("mysql"));
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-console.log('SECRET:', process.env.PASSWORD_DATABASE);
+console.log('SECRET:', process.env.SECRET);
 console.log('USER_DATABASE:', process.env.USER_DATABASE);
 console.log('PASSWORD_DATABASE:', process.env.PASSWORD_DATABASE);
-console.log('DATABASE:', process.env.PASSWORD_DATABASE);
-console.log('HOST_DATABASE:', process.env.PASSWORD_DATABASE);
-console.log('PORT_DATABASE:', process.env.PASSWORD_DATABASE);
+console.log('DATABASE:', process.env.DATABASE);
+console.log('HOST_DATABASE:', process.env.HOST_DATABASE);
+console.log('PORT_DATABASE:', process.env.PORT_DATABASE);
 const pool = mysql_1.default.createPool({
 	"user": process.env.USER_DATABASE,
 	"password": process.env.PASSWORD_DATABASE,
